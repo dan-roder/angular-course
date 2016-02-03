ccda1657f097c71cb5f8e65308fe75de2b6dc661
Dan Roder <droder@repequity.com>
2016-02-02 22:42:31 -0500

    common directives

--- app.js
+++ app.js
@@ -3,37 +3,20 @@ var myApp = angular.module('myApp', []);
 myApp.controller('mainController', ['$scope', '$log', '$timeout', '$filter',
   function($scope, $log, $timeout, $filter) {
 
-    /*
-     * Scope and Interpolation/Use of $timeout
-
-      $scope.name = 'Dan';
-
-      $timeout(function(){
-
-        $scope.name = 'Everybody';
-
-      }, 3000);
-    */
-
-    // Directives
     $scope.handle = '';
     $scope.lowercasehandle = function(){
       return $filter('lowercase')($scope.handle);
     }
 
-    $scope.$watch('handle', function(newValue, oldValue) {
+    $scope.characters = 5;
 
-      $log.info('Changed');
-      $log.log('Old: ' + oldValue);
-      $log.log('New:' + newValue);
+    $scope.rules = [
 
-    });
+      { rulename: 'Must be 5 characters' },
+      { rulename: 'Must not be used elsewhere' },
+      { rulename: 'Must be cool' }
+    ];
 
-    $timeout(function(){
-
-      $scope.handle = 'newtwitterhandle';
-      $log.log('Scope changed');
-
-    }, 3000);
+    $log.log($scope.rules);
 
 }]);
